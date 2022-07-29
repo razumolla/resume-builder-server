2c55b23999b44a7cbb8333a73a02234303940b8e
razumolla <rajuahmed9344@gmail.com>
2022-07-29 11:33:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const app = express()
+const port = process.env.PORT || 5000
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cdirk.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log("Database Connected");
+        const serviceCollection = client.db("resume_builder").collection("services");
+
+        
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello From Resume')
+})
+
+app.listen(port, () => {
+    console.log(`Resume-Builder app listening on port ${port}`)
+})
