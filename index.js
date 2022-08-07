3d137612bd696739d35479333e9f0479b3883603
razumolla <rajuahmed9344@gmail.com>
2022-08-07 10:55:36 +0600

    update

--- index.js
+++ index.js
@@ -6,22 +6,21 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
-// Middleware
+// Middleware on
 app.use(cors());
 app.use(express.json());
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.cdirk.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n68kq.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect();
-        console.log("Database Connected");
+        console.log("Database Connected 2");
         const serviceCollection = client.db("resume_builder").collection("services");
 
-        
     }
     finally {
 
