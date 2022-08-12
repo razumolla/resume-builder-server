899f50fd189a00171d5d9a4fbaeb6805c0cbf48b
syedmehedi0003 <mehedihasanbahar.com>
2022-08-12 16:45:28 +0600

    new commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -69,6 +70,7 @@ async function run() {
             const result = await cursor.toArray();
             res.send(result);
         })
+
         //POST data from cv
         app.post('/cvInfo', async (req, res) => {
             const info = req.body;
