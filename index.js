d2cab2df74bce8492804874bf37f0e3cc4d0838c
rabiulhossen <rhsimon69@gmail.com>
2022-08-22 01:51:00 +0600

    admin role added

--- index.js
+++ index.js
@@ -82,30 +82,41 @@ async function run() {
         })
 
 
-app.get("/user", async (req, res) => {
+app.get("/user",verifyJWT, async (req, res) => {
     const users = await userCollection.find().toArray();
     res.send(users);
   });
 
-  app.get("/admin/:email", async (req, res) => {
+//   see admin role 
+  app.get("user/admin/:email", async (req, res) => {
     const email = req.params.email;
     const user = await userCollection.findOne({ email: email });
     const isAdmin = user.role === "admin";
-    res.send({ admin: isAdmin });
+    res.send({admin: isAdmin} );
   });
 
-  app.put("/user/admin/:email", async (req, res) => {
+//   make admin 
+  app.put("/user/admin/:email",verifyJWT, async (req, res) => {
     const email = req.params.email;
-    const filter = await userCollection.findOne({ email: email });
-    console.log(filter);
+    const receiver = req.decoded.email;
+    const requestEmail = await userCollection.findOne({ email: receiver });
+   
+if(requestEmail.role === 'admin'){
+    const filter ={email :email};
     const updateDoc = {
-      $set: { role: "admin" },
-    };
+        $set: { role: "admin" },
+      };
+       
     const result = await userCollection.updateOne(filter, updateDoc);
     res.send(result);
+}
+else{
+    res.status(403).send({message: 'forbidden'});
+}
+   
   });
 
- // Delete
+ // Delete admin 
  app.delete("/user/:id", async (req, res) => {
     const id = req.params.id;
     const query ={_id:ObjectId(id)};
