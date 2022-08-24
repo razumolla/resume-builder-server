869da61f1a93e116ed8d751a3224fb949091adaa
razumolla <rajuahmed9344@gmail.com>
2022-08-24 10:15:35 +0600

    CL details added

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 
-
 // jwt middleware
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
